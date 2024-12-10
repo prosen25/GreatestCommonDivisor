edb52932b2219e55ad92aebb43118070441b243e
Prosenjit Mondal <contact.prosen25@gmail.com>
2024-12-10 17:50:59 +0000

    added login to find greatest common divisor of two number

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println(getGreatestCommonDivisor(25, 15));
+        System.out.println(getGreatestCommonDivisor(12, 30));
+        System.out.println(getGreatestCommonDivisor(9, 18));
+        System.out.println(getGreatestCommonDivisor(81, 153));
+    }
+
+    public static int getGreatestCommonDivisor(int first, int second) {
+
+        if (first < 10 || second < 10) {
+            return -1;
+        }
+
+        int smallNumber = Math.min(second, first);
+
+        for (int i = smallNumber; i > 0; i--) {
+            if (first % i == 0 && second % i == 0) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
